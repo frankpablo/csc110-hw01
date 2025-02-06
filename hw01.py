1ad03a1c8d850b0bedf3858b9ad23f382d214f64
Pablo Frank <frankpablo@gmail.com>
2025-02-06 12:58:57 -0500

    Create hw01.py

---
+++ hw01.py
@@ -0,0 +1,61 @@
+# ------------------------------------------------------
+#        Name: (put your name here)
+#       Peers: (add any collaborators)
+#  References: (anything you checked to solve this)
+# ------------------------------------------------------
+
+# ========== Setup for HW. DO NOT MODIFY ======
+x=0
+y=0
+a=0
+b=0
+c=0
+result1 = 0
+result2 = 0
+result3 = 0
+result4 = 0
+result5 = 0
+# End of Setup code ---------------------------
+
+
+
+# Part 1: Basic Operations
+# =============================================
+# Your code for part 1 under this line and before the print statements
+
+print("Part 1: x =",x)
+print("Part 1: y =",y)
+print("Part 1: a =",a)
+print("Part 1: b =",b)
+print("Part 1: c =",c)
+print("Part 1: result =", result1)
+# End of Part 1 ----------------------
+
+
+# Part 2: Power
+# =============================================
+# Your code for part 2 under this line and before the print statements
+
+print("Part 2: x =",x)
+print("Part 2: y =",y)
+print("Part 2: result =", result2)
+# End of Part 2 ----------------------
+
+
+
+# Part 3: Integer divide
+# =============================================
+# Your code for part 3 under this line and before the print statements
+
+print("Part 3: a =",a)
+print("Part 3: b =",b)
+print("Part 3: result =", result3)
+# End of Part 3 ----------------------
+
+
+# Part 4: Modulo
+# =============================================
+# Your code for part 4 under this line and before the print statements
+
+print("Part 4: result =", result4)
+# End of Part 4 ----------------------
