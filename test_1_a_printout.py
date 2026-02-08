eccd208f5deec8d20b4f584aab3e3f93f6b61f47
Pablo Frank <frankpablo@gmail.com>
2026-02-08 11:10:25 -0500

    updated function names

--- test_1_a_printout.py
+++ test_1_a_printout.py
@@ -1,7 +1,7 @@
 import hw01  # Import the module here
 import sys
 
-def test1_a_printout(capsys):
+def test_1_a_printout(capsys):
     hw01.main()
     captured = capsys.readouterr()
     assert "Part 1: a = 1.5" in captured.out, "Tip: check the printout for a is EXACT"
--- test_2_y_printout.py
+++ test_2_y_printout.py
@@ -1,7 +1,7 @@
 import hw01  # Import the module here
 import sys
 
-def test2_y_printout(capsys):
+def test_2_y_printout(capsys):
     hw01.main()
     captured = capsys.readouterr()
     assert "Part 2: y = -3" in captured.out, "Tip: check the printout for y is EXACT"
