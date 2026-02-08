7410b80b4482fc0f8c5c9bd375111fbe75a1162c
Michael Robson <mrobson@smith.edu>
2026-02-08 07:54:57 -0500

    test: Match function names to updated filenames (add _)

--- test_1_y_printout.py
+++ test_1_y_printout.py
@@ -1,7 +1,7 @@
 import hw01  # Import the module here
 import sys
 
-def test1_y_printout(capsys):
+def test_1_y_printout(capsys):
     hw01.main()
     captured = capsys.readouterr()
     assert "Part 1: y = 1" in captured.out, "Tip: check the printout for y is EXACT"
--- test_2_x_printout.py
+++ test_2_x_printout.py
@@ -1,7 +1,7 @@
 import hw01  # Import the module here
 import sys
 
-def test2_x_printout(capsys):
+def test_2_x_printout(capsys):
     hw01.main()
     captured = capsys.readouterr()
     assert "Part 2: x = 5" in captured.out, "Tip: check the printout for x is EXACT"
